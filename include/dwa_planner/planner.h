8168fbe3b5bcbfe131ca6f4dd6cb6112bdb1dff4
smitdumore <smit.dumore17@vit.edu>
2022-10-30 13:54:25 -0400

    build with eigen dependency works

--- include/dwa_planner/planner.h
+++ include/dwa_planner/planner.h
@@ -2,7 +2,6 @@
 #define __PLANNER_H
 
 #include <ros/ros.h>
-/*
 #include <tf/tf.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseStamped.h>
@@ -13,7 +12,7 @@
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Twist.h>
-
+/*
 class Planner{
     private:
 
